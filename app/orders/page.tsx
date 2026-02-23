d21daeffe71f9d94bd18212f0f5c14bc3fc3d41e
Isaac Vargas <hello@isaacvargas.dev>
2026-02-23 09:24:05 -0800

    🎨 add: qa orders page

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -2,6 +2,7 @@ import { WorkflowStatus } from "@/prisma/generated/enums"
 import OrdersPending from "./_components/pending/Pending"
 import OrdersPicking from "./_components/picking/Picking"
 import OrdersPacking from "./_components/packing/Packing"
+import OrdersQA from "./_components/qa/QA"
 
 type Props = {
   searchParams: Promise<{
@@ -20,6 +21,8 @@ const Orders = async ({ searchParams }: Props) => {
       return <OrdersPicking />
     case 'PACKING':
       return <OrdersPacking />
+    case 'QA':
+      return <OrdersQA />
     default:
       return false;
   }
